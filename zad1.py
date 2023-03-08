e01679f25a957fb445ce7d87487088fab22cf6d2
MarkoBuu <marko.budak33@gmail.com>
2023-03-08 17:04:05 +0100

    lv1

---
+++ zad1.py
@@ -0,0 +1,8 @@
+def total():
+    return n * s
+
+s = float(input ("Odrađeno sati: "))
+n = float(input("Satnica: "))
+
+print("Ukupna zarada je: " + str(total()))
+    
---
+++ zad2.py
@@ -0,0 +1,12 @@
+br = float(input("Unesite broj: "))
+
+if br >= 0.9 :
+    print("A")
+elif br >= 0.8:
+    print("B")
+elif br >=0.7:
+    print("C")
+elif br >= 0.6:
+    print("D")
+else :
+    print("F")
